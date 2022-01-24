0292f5064b6d83a4704d7b77de61a7f3f34f6e7f
Song-EunJu <jd06280@naver.com>
2022-01-24 11:22:57 +0900

    [SETTING] RDS - TypeORM 연동

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Board } from './boards.entity';
+import { BoardRepository } from './boards.repository';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+    constructor(
+        @InjectRepository(BoardRepository) // boardservice 안에서 boardrepository 사용하기 위해서
+        private boardsRepository: BoardRepository){
+        }
+    
+    async getAllBoards(): Promise <Board[]> {
+        return this.boardsRepository.find();
+    }
+
+    createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
+        return this.boardsRepository.createBoard(createBoardDto);
+    }
+}
