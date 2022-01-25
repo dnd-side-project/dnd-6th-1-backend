b6ba4cee5321d192397929818c9609e815401d2b
Song-EunJu <jd06280@naver.com>
2022-01-25 22:06:07 +0900

    [TEST] 서버 재접속 후 S3 테스트

--- src/board-images/board-images.controller.ts
+++ src/board-images/board-images.controller.ts
@@ -5,14 +5,14 @@ import * as multerS3 from 'multer-s3';
 import { FileInterceptor} from '@nestjs/platform-express';
 
 // AWS S3
-// const s3 = new AWS.S3();
+const s3 = new AWS.S3();
 // AWS.config.update({
 //     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
 //     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 //     region: process.env.AWS_REGION
 // });
 
-// let temp=""
+let temp=""
 @Controller('board-images')
 export class BoardImagesController {
     constructor(
@@ -25,21 +25,21 @@ export class BoardImagesController {
         return this.boardImagesService.getHello2();
     }
 
-    // @Post()
-    // @UseInterceptors(FileInterceptor('file', {
-    //   storage: multerS3({
-    //     s3: s3,
-    //     bucket: process.env.AWS_S3_BUCKET_NAME,
-    //     contentType: multerS3.AUTO_CONTENT_TYPE,
-    //     acl: 'public-read',
-    //     key: function (request, file, cb) {
-    //         temp = `${Date.now().toString()}-${file.originalname}`;
-    //         console.log(file.ContentType)
-    //         cb(null, temp);
-    //     }
-    //   })
-    // }))
-    // async uploadFile(@UploadedFile() file: Express.Multer.File) {
-    //     return await this.boardImagesService.uploadFile(file, temp);
-    // }
+    @Post()
+    @UseInterceptors(FileInterceptor('file', {
+      storage: multerS3({
+        s3: s3,
+        bucket: process.env.AWS_S3_BUCKET_NAME,
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        acl: 'public-read',
+        key: function (request, file, cb) {
+            temp = `${Date.now().toString()}-${file.originalname}`;
+            console.log(file.ContentType)
+            cb(null, temp);
+        }
+      })
+    }))
+    async uploadFile(@UploadedFile() file: Express.Multer.File) {
+        return await this.boardImagesService.uploadFile(file, temp);
+    }
 }
