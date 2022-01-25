331a003afc00fd8301e0e2662e38540de4a54033
Song-EunJu <jd06280@naver.com>
2022-01-25 13:45:20 +0900

    [CHORE] S3 코드 주석 처리

--- src/board-images/board-images.entity.ts
+++ src/board-images/board-images.entity.ts
@@ -1,16 +1,13 @@
-import { BaseEntity, Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+// import { BaseEntity, Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
-@Entity()
-export class BoardImages extends BaseEntity{
-    @PrimaryGeneratedColumn()
-    boardImageId: number;
+// @Entity()
+// export class BoardImages extends BaseEntity{
+//     @PrimaryGeneratedColumn()
+//     boardImageId: number;
 
-    // @Column()
-    // boardId: number;
+//     @Column()
+//     originalName: string;
 
-    @Column()
-    originalName: string;
-
-    @Column({ comment: "s3 업로드된 localtion url" })
-    imageUrl: string;
-}
+//     @Column({ comment: "s3 업로드된 localtion url" })
+//     imageUrl: string;
+// }
