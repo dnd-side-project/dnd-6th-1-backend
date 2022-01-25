4fa10b00e007555665dce6c49b202ca1fa84498e
Song-EunJu <jd06280@naver.com>
2022-01-25 16:32:43 +0900

    [CHORE] AWS update 코드 주석 처리

--- src/board-images/board-images.service.ts
+++ src/board-images/board-images.service.ts
@@ -6,12 +6,7 @@ import { BoardImagesRepository } from './board-images.repository';
 require("dotenv").config();
 
 // AWS S3
-const s3 = new AWS.S3();
-AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION
-});
+// const s3 = new AWS.S3();
 
 @Injectable()
 export class BoardImagesService {
@@ -33,6 +28,12 @@ export class BoardImagesService {
                 Bucket: process.env.AWS_S3_BUCKET_NAME,
                 Key: temp
             }
+            const s3 = new AWS.S3();
+            // AWS.config.update({
+            //     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+            //     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+            //     region: process.env.AWS_REGION
+            //   });
             s3.getObject(params, function(err,data){
                 if(err){
                     throw err;
