516e99b1e93d25f48ae92ae0297e4f53d3aec1b3
kkbo5222 <kkbo5222@gmail.com>
2022-02-03 14:18:44 +0900

    [ADD] : 회원가입 API Swagger 추가
    - issue: #9

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,15 +4,18 @@ import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { GetUser } from './get-user.decorator';
 import { User } from './user.entity';
-
+import { ApiTags, ApiOperation, ApiResponse, ApiCreatedResponse } from '@nestjs/swagger';
 
 
 
 @Controller('user')
+@ApiTags('유저 API')
 export class AuthController {
     constructor( private authService: AuthService){}
 
     @Post('/signup')
+    @ApiOperation({ summary: '회원가입 API', description: '이메일, 비밀번호, 닉네임 입력' })
+    @ApiCreatedResponse({ description: '유저를 생성합니다', type: User })
     signUp(@Body(ValidationPipe) authcredentialsDto: AuthCredentialsDto): Promise<void> {
         return this.authService.signUp(authcredentialsDto);
     }
