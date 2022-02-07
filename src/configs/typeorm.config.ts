82510ba00055044d513166e73382360e2edbd2c3
Song-EunJu <jd06280@naver.com>
2022-02-07 15:55:43 +0900

    [CHORE]: config에 timezone 설정

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -11,7 +11,8 @@ export const typeORMConfig : TypeOrmModuleOptions = {
     database: dbConfig.database,
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
     // 엔티티를 이용해서 DB 테이블 생성해주므로 엔티티 파일이 어디에 있는지 설정해줌
-    synchronize: dbConfig.synchronize
+    synchronize: dbConfig.synchronize,
     // 애플리케이션을 다시 실행할 때 엔티티 안에서 수정된 컬럼의 길이, 타입, 변경값들을
     // 해당 테이블을 drop한 후 다시 생성해준다.
+    timezone: 'Asia/Seoul'
 }
